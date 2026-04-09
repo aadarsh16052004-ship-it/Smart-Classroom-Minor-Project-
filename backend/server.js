17c28c88ef587e1ecebaf30b54d4282ff9259820
aadarsh16052004-ship-it <aadarsh16052004@gmail.com>
2026-04-10 01:56:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-
-// import authRoutes from "./routes/authRoutes.js";
-// import studentRoutes from "./routes/studentRoutes.js";
-// import teacherRoutes from "./routes/teacherRoutes.js";
-// import adminRoutes from "./routes/adminRoutes.js";
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// /* ROUTES */
-// app.use("/api/auth", authRoutes);
-// app.use("/api/student", studentRoutes);
-// app.use("/api/teacher", teacherRoutes);
-// app.use("/api/admin", adminRoutes);
-// export default app;
-
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import app from "./app.js";
